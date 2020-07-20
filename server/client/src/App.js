0924467fead1d89d04bd170b092cf37b889621ac
Bharath-pg <bharathpg1997@gmail.com>
2020-07-20 15:54:10 +0530

    react with server

---
+++ server/client/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import './assets/css/main.css';
+
+function App() {
+  return (
+    
+    <div class="p-4 m-4 bg-green-600">
+        <h1 class="text-2xl font-bold text-black">Tailwind CSS Demo</h1>
+    </div>
+    
+
+);
+}
+
+export default App;
