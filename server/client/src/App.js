2c501342ba8f3ac22fa81de860f1bffb9c64eb34
Bharath-pg <bharathpg1997@gmail.com>
2020-07-20 21:12:47 +0530

    change bg

--- server/client/src/App.js
+++ server/client/src/App.js
@@ -5,8 +5,8 @@ import './assets/css/main.css';
 function App() {
   return (
     
-    <div class="p-4 m-4 bg-green-600">
-        <h1 class="text-2xl font-bold text-black">Tailwind CSS Demo</h1>
+    <div class="p-4 m-4 bg-gray-600">
+        <h1 class="text-2xl font-bold text-gray-200">Simple React</h1>
     </div>
     
 
